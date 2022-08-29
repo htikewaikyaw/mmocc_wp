8c2264d81fa652bffa3e2b550f682976a9fef4e7
wintunmmocc <wintun.mmocc@gmail.com>
2022-08-29 16:22:58 +0630

    update to beta-release-1.0

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'mmocc_wp' );
+define( 'DB_NAME', 'mmoccbackuptest23' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
